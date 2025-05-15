80ebc418f505378bf1b013b6005fefd9d27c4ea9
Skiel221 <ezepelo1406@gmail.com>
2025-05-15 11:21:00 -0300

    avances en la seccion de  contacto
    che juan, fijate que queda raro

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import './css/contact.css';
 import Hero from '../components/Hero';
+import { FaBusAlt } from "react-icons/fa";
+import { FaTrainSubway } from "react-icons/fa6";
 
 export default function Contact() {
   return (
@@ -8,14 +10,28 @@ export default function Contact() {
         <h2>Contactos</h2>
         <p>Nuestros contactos</p>
       </Hero>
-      
-      <div className='contacto'>
-        <h2>¿Donde nos encontramos?</h2>
-        <h4>Güemes 2051, B1842 Monte Grande, Provincia de Buenos Aires</h4>
-      </div>
-      <div className='transporte'>
-        <h2>Transportes que te acercan a la tecnica</h2>
-
+      <div className='contact-section'>
+        <div className='contacto'>
+          <h2>¿Donde nos encontramos?</h2>
+          <h4>Güemes 2051, B1842 Monte Grande, Provincia de Buenos Aires</h4>
+        </div>
+        <h2>Transporte que te acercan a la técnica</h2>
+        <div className='trans'>
+          <div className='transporte'>
+            <FaBusAlt size={60} />
+            <div className='lineas'>
+              <h4>Líneas de colectivo</h4>
+              <li>501 - ramal 4.</li>
+              <li>501 - ramal 5.</li></div>
+           </div>
+          <div className='transporte'>
+            <FaTrainSubway size={60} />
+            <div className='lineas'>
+              <h4>Líneas de tren</h4> <br />
+              <li>Línea Roca, Tren Ezeiza.</li>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
